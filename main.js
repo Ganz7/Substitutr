5de13bab5c63ed6020ef8b65e1900f5c91513676
Ganz7 <ganzse7en@gmail.com>
2016-06-01 02:04:52 -0500

    Get words to be substituted from chrome.storage

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // Update HTML
 
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Ganesh", "g"), "Ganz");
+chrome.storage.sync.get("wordsObj", function(obj){
+	alert("Object read - " + obj);
+	for(var wKey in obj){
+		document.body.innerHTML = document.body.innerHTML.replace(new RegExp(wKey, "g"), obj[wKey]);
+	}
+});
+//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Ganesh", "g"), "Ganz");
---
+++ options.js
@@ -0,0 +1,17 @@
+
+function save_options(){
+	var wordSubstitutions = document.getElementById('sList').value;
+	chrome.storage.sync.set({
+    	"wordsObj": wordSubstitutions
+	}, function() {
+	    // Update status to let user know options were saved.
+	    var status = document.getElementById('status');
+	    status.textContent = 'Options saved.';
+	    setTimeout(function() {
+	      status.textContent = '';
+	    }, 750);
+	});
+
+}
+
+document.getElementById('save').addEventListener('click', save_options);
