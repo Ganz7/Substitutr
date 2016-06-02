4ebb3a642ec87dcc1f6985408d8478f286546fb5
Ganz7 <ganzse7en@gmail.com>
2016-06-01 23:03:00 -0500

    Minor

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-// Update HTML
+// Update the HTML page
 
-chrome.storage.sync.get("wordsObj", function(obj){
-	console.log("Object read - " + obj);
-	var DOM = document.body.innerHTML;
-	for(var wKey in obj){
-		DOM = DOM.replace(new RegExp(wKey, "g"), obj[wKey]);
+chrome.storage.sync.get('wordsObj', function(data){
+	if (!chrome.runtime.error) {
+
+		/* Get the object from the JSON String*/
+		var list = JSON.parse(data.wordsObj);
+		/* Get the HTML of the page */
+		var DOM = document.body.innerHTML;
+
+		/* Replace each of the words to be substituted from the list */
+		for(var wKey in list){
+			DOM = DOM.replace(new RegExp(wKey, "g"), list[wKey]);
+		}
+
+		/* Update the page */
+		document.body.innerHTML = DOM;
 	}
-	document.body.innerHTML = DOM;
 });
-//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Ganesh", "g"), "Ganz");
